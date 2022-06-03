58f275687dc658f1dbd2c24b395b7b253f4c2651
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-03 18:27:44 +0200

    added pictures and other data for the cards

--- src/components/data/api.js
+++ src/components/data/api.js
@@ -1,27 +1,31 @@
 import React from "react";
+import beverages from "../../images/beverages.jpg";
+import bookpalace from "../../images/bookpalace.jpg";
+import holidaze from "../../images/holidaze.jpg";
 
 export const projectData = [
   {
     title: "Beverages Inc.",
-    description: "A site for finding different beverages",
-    screenshot: "",
-    github: "",
+    description:
+      "A site for finding different beverages. Users are able to toggle their favorite beverages in and out of their favorite list.",
+    screenshot: beverages,
+    github: "https://github.com/Kaladinge/js-frameworks-course-assignment",
     livesite: "",
   },
   {
     title: "Book Palace",
     description:
       "A site for book enthusiasts. Customers are able to search for and browse through books and add them to the cart. Admin users can add new books and edit/delete existing ones.",
-    screenshot: "",
-    github: "",
-    livesite: "",
+    screenshot: bookpalace,
+    github: "https://github.com/Kaladinge/book-palace",
+    livesite: "https://bookpalace-project.netlify.app",
   },
   {
     title: "Holidaze",
     description:
       "A site for travelers to Bergen who want to find a place to stay in Bergen. Users can book from a variety of hotels, B & Bs and guesthouses. Admin users can respond to these book enquiries and also add new accommodations, as well as edit/delete existing ones",
-    screenshot: "",
+    screenshot: holidaze,
     github: "",
-    livesite: "",
+    livesite: "https://holidaze-larsinge.netlify.app",
   },
 ];
--- src/components/home/ProjectCards.js
+++ src/components/home/ProjectCards.js
@@ -4,50 +4,33 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { projectData } from "../data/api";
 
 export default function ProjectCards() {
   return (
     <Container>
-      <Row className="justify-content-md-between border">
-        <Col className="border">
-          <Card className="w-100">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="border">
-          <Card className="w-100">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="border">
-          <Card className="w-100">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Col>
+      <Row className="mt-5 justify-content-between">
+        {projectData.map((item) => (
+          <Col lg={4} key={item.title} className="mb-4">
+            <Card className="w-100 h-100 position-relative">
+              <Card.Img variant="top" src={item.screenshot} />
+              <Card.Body className="d-flex flex-column justify-content-between">
+                <div>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>{item.description}</Card.Text>
+                </div>
+                <div className="d-flex justify-content-around">
+                  <a href={item.github} className="">
+                    Github
+                  </a>
+                  <a href={item.livesite} className="">
+                    Live site
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
